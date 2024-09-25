f4811db308b33144489f827c4aa53b848089c2db
junwei-wong <weijwong@gmail.com>
2024-09-25 08:49:26 +0800

    Added styling for clickable icons

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const shareButton = document.getElementById("share-icon");
+const shareIcon = document.getElementById("img-share");
+const popover = document.getElementById("popover");
+shareButton.addEventListener("click", function (e) {
+  console.log(e);
+  popover.showPopover();
+});
+
+shareButton.addEventListener("mouseover", function (e) {
+  // shareIcon.
+});
